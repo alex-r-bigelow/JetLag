e1508942e360562ba15143f2b18f568219bf7de4
Steven R. Brandt <sbrandt@cct.lsu.edu>
2020-03-23 14:07:45 -0500

    Add git pulls

--- docker/start.sh
+++ docker/start.sh
@@ -1,2 +1,8 @@
-(cd /traveler-integrated && python3 serve.py > ./serve.log 2>&1)
+HERE=${PWD}
+cd /traveler-integrated
+git pull
+cd /JetLag
+git pull
+cd $HERE
+(cd /traveler-integrated && python3 serve.py > ./serve.log 2>&1) &
 jupyter notebook --ip 0.0.0.0 --port 8789 --no-browser
