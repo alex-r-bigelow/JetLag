655de583a6e1a9580e6ef493f3a59d7eda785416
Steven R. Brandt <sbrandt@cct.lsu.edu>
2020-06-25 13:10:16 -0500

    Copy the Demo file in.

--- docker/start.sh
+++ docker/start.sh
@@ -8,5 +8,9 @@ git pull
 cd /JetLag
 git pull
 cd $HERE
+if [ ! -r Demo.ipynb ]
+then
+    cp /JetLag/notebooks/Demo.ipynb .
+fi
 (cd /traveler-integrated && python3 serve.py > ./serve.log 2>&1) &
 jupyter notebook --ip 0.0.0.0 --port 8789 --no-browser
